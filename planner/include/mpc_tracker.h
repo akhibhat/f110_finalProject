23191fd243790aa365a563a863c0b831a4d89504
Mihir <mihir.parmar1998@gmail.com>
2020-04-21 22:40:27 -0400

    prediction + mpc header

---
+++ planner/include/mpc_tracker.h
@@ -0,0 +1,24 @@
+#include <ros/ros.h>
+#include <ros/package.h>
+#include <sensor_msgs/LaserScan.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Point.h>
+#include <ackermann_msgs/AckermannDriveStamped.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <tf2_ros/transform_listener.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+
+#include <math.h>
+#include <vector>
+#include <array>
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <string>
+#include <algorithm>
+#include <boost/algorithm/string.hpp>
+
+
